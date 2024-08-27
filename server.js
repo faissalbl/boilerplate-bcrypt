79bdf5663d196a420cca86e39091b2f119874d7c
Faissal Lauar <faissalbl@gmail.com>
2024-08-27 14:00:22 +0000

    code complete

--- server.js
+++ server.js
@@ -11,15 +11,27 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds).then(hash => {
+    console.log(hash);
+    // at this point we would save this hash to the database
+    bcrypt.compare(myPlaintextPassword, hash).then(res => {
+        console.log(res);
+    });
+});
 
-
-
+// bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+//     console.log(hash);
+//     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+//         console.log(res);
+//     });
+// });
 //END_ASYNC
 
 //START_SYNC
-
-
-
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
 
 
